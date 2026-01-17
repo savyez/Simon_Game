c4b14227085ef74fa0ecb0d72a9377f826aee2cf
Savyez <visnuk252@gmail.com>
2026-01-17 18:00:49 +0530

    Sound Integrated

--- game.js
+++ game.js
@@ -6,9 +6,10 @@ var userClickedPattern = [];
 
 $(".btn").click(function() {
 
-  var userChosenColor = $(this).attr("id");
-  userClickedPattern.push(userChosenColor);
-  console.log(userClickedPattern);
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+    playSound(userChosenColor);
 
 });
 
@@ -21,7 +22,11 @@ function nextSequence() {
     console.log(gamePattern);
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("./sounds/" + randomChosenColor + ".mp3");
-    audio.play();
+    playSound(randomChosenColor);
+}
 
+
+function playSound(color) {
+    var audio = new Audio("./sounds/" + color + ".mp3");
+    audio.play();
 }
