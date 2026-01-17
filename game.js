a372e00a880873de4c9767c887f1be8f6aba36c9
Savyez <visnuk252@gmail.com>
2026-01-17 17:18:01 +0530

    Created New Pattern

---
+++ game.js
@@ -0,0 +1,10 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    console.log(gamePattern);
+}
